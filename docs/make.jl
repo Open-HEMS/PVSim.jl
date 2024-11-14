9fc2e3487fbe163e3c66c956f20ed93fbf22bb67
Stefan de Lange <langestefan@msn.com>
2024-11-14 23:20:48 +0100

    Setup project

---
+++ docs/make.jl
@@ -0,0 +1,21 @@
+using PVSim
+using Documenter
+
+DocMeta.setdocmeta!(PVSim, :DocTestSetup, :(using PVSim); recursive = true)
+
+const page_rename = Dict("developer.md" => "Developer docs") # Without the numbers
+const numbered_pages = [
+    file for file in readdir(joinpath(@__DIR__, "src")) if
+    file != "index.md" && splitext(file)[2] == ".md"
+]
+
+makedocs(;
+    modules = [PVSim],
+    authors = "Stefan de Lange",
+    repo = "https://github.com/Open-HEMS/PVSim.jl/blob/{commit}{path}#{line}",
+    sitename = "PVSim.jl",
+    format = Documenter.HTML(; canonical = "https://Open-HEMS.github.io/PVSim.jl"),
+    pages = ["index.md"; numbered_pages],
+)
+
+deploydocs(; repo = "github.com/Open-HEMS/PVSim.jl")
---
+++ test/test-basic-test.jl
@@ -0,0 +1,3 @@
+@testset "PVSim.jl" begin
+    @test PVSim.hello_world() == "Hello, World!"
+end
